951f0af40a820e711aa41d5d321ac55fd288f60d
Davidsoncole <davidson4ice@yahoo.com>
2019-09-07 11:31:08 +0100

    modified server.js file

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const express = require("express");
+const port = process.env.PORT || 8080;
+const app = express();
 
-const app = express()
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+    res.sendfile(__dirname + "/dist/index.html");
+});
+app.listen(port);
 
-// serves files from our dist directory which now contains out index.html file
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
-
-const port = process.env.PORT || 8080
-app.listen(port)
-
-console.log('Listening on port: ' + port)
+console.log("Server started...");
